97ae1bb01d8a10339e614e679ba047aa072da64f
jolantalei <jolanta.leigute@gmail.com>
2019-04-29 18:15:36 +0300

    commits

--- ClassroomProject/src/Main.java
+++ ClassroomProject/src/Main.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String question1 = "Which one of these is OOP programming language?\n" +
+                "(a)Java\n(b)R\n(c)SQL";
+        String question2 = "Which one of these belongs to primitive data type?\n" +
+                "(a)String\n(b)long\n(c)Array";
+
+        Question [] questions = {new Question (question1, "a"),new Question (question2, "b"),
+        };
+        takeTest (questions);
+    }
+
+    public static void takeTest (Question[] questions){
+        int score = 0;
+        Scanner userInput = new Scanner(System.in);
+
+        for (int i = 0; i < questions.length; i++){
+            System.out.println(questions[i].prompt);
+            String answer = userInput.nextLine();
+            if (answer.equals((questions[i].answer))){
+                score++;
+            }
+        }
+        System.out.println("You got " + score + "/" + questions.length);
     }
 }
+//input.equals.Ignore Case ("java") - ignore case ignorē ar kādu burtu lielumu ievadīts lasa tikai nozīmi
--- ClassroomProject/src/UserInterface.java
+++ ClassroomProject/src/UserInterface.java
@@ -1,4 +1,21 @@
-public class UserInterface {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class UserInterface extends Application {
+
+    private Stage window;
 
 
+
+    public static void main (String[]args){
+
+        launch(args);
+    }
+
+
+    public void start (Stage primaryStage) throws Exception {
+        window = primaryStage;
+
+
+    }
 }
